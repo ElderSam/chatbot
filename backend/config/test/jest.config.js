c2bc7a76cf2f5d52a257727b326a5087b00cba16
ElderSam <eldersamuel98@gmail.com>
2025-08-24 00:02:18 -0300

    refactor: reorganize project structure for better maintainability
    - Create /infrastructure for Docker and Kubernetes configs
    - Organize /backend/config with env/, test/, and app/ folders
    - Move environment files to /backend/config/env/
    - Move test configurations to /backend/config/test/
    - Update all scripts and documentation references
    - Consolidate Docker and K8s manifests in /infrastructure
    - Improve separation of concerns between code and infrastructure

---
+++ backend/config/test/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '../../',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+    '!src/**/*.spec.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.dto.ts',
+    '!src/**/*.entity.ts',
+    '!src/**/*.module.ts',
+    '!src/main.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/config/test/jest.setup.ts'],
+};
