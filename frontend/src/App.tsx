65922e51120c27ee218f5b15261b0267920f604e
ElderSam <eldersamuel98@gmail.com>
2025-08-25 02:07:03 -0300

    feat: frontend - set up routing with RouterProvider and define routes for HomePage and UserPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,47 @@
-import './App.css';
+import {
+  RouterProvider,
+  createRouter,
+  createRoute,
+  createRootRoute,
+  Outlet,
+} from '@tanstack/react-router';
 
+import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
 
+import './App.css';
+
+// Root route
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      {/* <div style={{position: 'fixed', top: 0, left: 0, background: '#ff0', color: '#000', zIndex: 9999, padding: 4}}>
+        RootRoute Rendered
+      </div> */}
+      <Outlet />
+    </>
+  ),
+});
+
+// Child routes
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: HomePage,
+});
+
+const createUserRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/create-user',
+  component: UserPage,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, createUserRoute]);
+
+const router = createRouter({ routeTree });
+
 function App() {
-  return <UserPage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
